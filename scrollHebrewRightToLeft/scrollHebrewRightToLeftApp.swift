fe24c324a1dbc341afb54c0f2c6298d18c82764f
Steven Hertz <StevenH@appi4.com>
2024-05-15 11:32:18 -0400

    Initial Commit

---
+++ scrollHebrewRightToLeft/scrollHebrewRightToLeftApp.swift
@@ -0,0 +1,17 @@
+//
+//  scrollHebrewRightToLeftApp.swift
+//  scrollHebrewRightToLeft
+//
+//  Created by Steven Hertz on 5/15/24.
+//
+
+import SwiftUI
+
+@main
+struct scrollHebrewRightToLeftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
